c739c641cf07606410b5e937b500e0ea1a485efc
David Carlisle <d.p.carlisle@gmail.com>
2016-01-22 22:09:17 +0000

    initial commit

---
+++ build.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env texlua
+
+-- Build script for "graphics-cfg" files
+
+-- Identify the bundle and module
+bundle = ""
+module = "graphics-cfg"
+
+sourcefiles =
+  {
+    "color.cfg","graphics.cfg"
+  }
+typesetfiles =
+  {
+  }
+
+-- No automated tests for this bundle
+testfildir = ""
+
+
+-- Find and run the build system
+kpse.set_program_name ("kpsewhich")
+dofile (kpse.lookup ("l3build.lua"))
